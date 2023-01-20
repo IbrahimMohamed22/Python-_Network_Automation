f8373ee64295123f31124109ae7d07a8f8342604
Ibrahim Hamad <ibrahimhamad951@gmail.com>
2023-01-20 02:18:01 +0200

    Reachability function added for main and monitor interface

--- scripts for netmiko/cisco_version.py
+++ scripts for netmiko/cisco_version.py
@@ -1,5 +1,7 @@
 # test point to point reachibilty
 # get vlan, vrf, and ip 
+# check reachibility for main and minitor interfaces
+
 
 import re
 from netmiko import ConnectHandler
@@ -22,21 +24,39 @@ net_connect = ConnectHandler(**device_info)
 net_connect.enable()                                             # to enter enable mode , require enable password .. in secret 
 output = net_connect.send_command('sh interface description | i ' + order_id )
 print (output)
-main_int = re.search('(\S+)',output).groups()
-output = net_connect.send_command('sh run interface ' + main_int[0])     # re.search return tuple so use main_int[0]   
-print (output)
-ip = re.search('address (\S+)',output).groups()
-vlan = re.search('dot1Q (\S+)',output).groups()
-vrf= re.search('forwarding (\S+)',output).groups()
-ip =ip[0].split('.')                                # convert list to string , (.) is delimater between each element
-if (int(ip[-1]) %2 == 0):                            # add or remove 1 from last digit in ip add to get point to point ip 
-    ip[-1] = int(ip[-1]) - 1
-    ip[-1] = str(ip[-1])                            # convert last digit to string again 
-else:
-    ip[-1] = int(ip[-1]) + 1
-    ip[-1] = str(ip[-1])    
 
-ip = '.'.join(ip)      # convert list to string again , (.) is delimater between each element)
-reachipility = net_connect.send_command('ping vrf ' + vrf[0] + " "+ ip)
-print(reachipility)
-print('Main Vlan is : ' + vlan[0])
+interfaces = re.findall('([GT]\S+)',output)                            # get giga interfaces (main and monitor) and put them in list 
+output = net_connect.send_command('sh run interface ' + interfaces[0])     # re.search return tuple so use main_int[0] 
+print (output)
+
+
+def check_reachibility():                                            # create function to check main mad monitor reachibility
+    ip = re.search('address (\S+)',output).groups()
+    vlan = re.search('dot1Q (\S+)',output).groups()
+    vrf= re.search('forwarding (\S+)',output).groups()
+    ip =ip[0].split('.')                                             # convert list to string , (.) is delimater between each element
+    if (int(ip[-1]) %2 == 0):                                        # add or remove 1 from last digit in ip add to get point to point ip 
+        ip[-1] = int(ip[-1]) - 1
+        ip[-1] = str(ip[-1])                                           # convert last digit to string again 
+    else:
+        ip[-1] = int(ip[-1]) + 1
+        ip[-1] = str(ip[-1])    
+
+    ip = '.'.join(ip)      # convert list to string again , (.) is delimater between each element)
+    reachipility = net_connect.send_command('ping vrf ' + vrf[0] + " "+ ip)
+    print(reachipility)
+
+
+check_reachibility()
+
+if (len(interfaces[1] > 0)):                                                  # check if there is main and monitor interface or not 
+    output = net_connect.send_command('sh run int ' + interfaces[1])
+    check_reachibility()
+
+
+physical_int = re.search('([GT]\S\S\S\S)',output).groups()                       # get main physical interface
+output = net_connect.send_command('sh run int ' + physical_int)
+print (output)
+output = net_connect.send_command('sh arp int '+ physical_int + '.400')            
+
+
